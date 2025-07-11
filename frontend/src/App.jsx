56c374e80f22c88aa6ec88d58380eefba80074dd
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2025-07-11 22:12:27 +1000

    init vite project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProfileCard from './components/ProfileCard.jsx'
+
+function App() {
+  return (<>
+    <ProfileCard ></ProfileCard>
+  </>)
+}
+
+export default App
